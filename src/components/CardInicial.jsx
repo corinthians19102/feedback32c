62943e410b8416e24c3ac619d229f28426f71a72
juliotimao5311 <juliotimao5311@gmail.com>
2025-05-06 02:19:52 +0000

    funcao de submit movida para o cardinicial e funcao de chatenote movida para input

--- src/components/CardInicial.jsx
+++ src/components/CardInicial.jsx
@@ -1,7 +1,16 @@
 import iconStar from "../assets/icon-star.svg"
 import { InputButton } from "./InputButton"
 
-export function CardInicial({ handleChangeRateNote, handleSubmit }) {
+export function CardInicial({ setRateNote, setSubmited, rateNote }) {
+  function handleSubmit() {
+    if (rateNote !== 0) {
+      setSubmited(true)
+      return
+    }
+
+    alert("Please, choose a rate button!")
+  }
+
     return (
         <div className="bg-radial-gradient max-w-103 px-6 pt-6 pb-8 rounded-2xl font-overpass">
         <div className="bg-dark-blue p-4 w-fit rounded-full mb-4">
@@ -13,11 +22,11 @@ export function CardInicial({ handleChangeRateNote, handleSubmit }) {
         <p className="text-light-grey text-sm leading-1 mb-6">Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!</p>
 
         <div className="flex justify-between mb-6">
-          <InputButton value={1} handleChangeRateNote={handleChangeRateNote} />
-          <InputButton value={2} handleChangeRateNote={handleChangeRateNote} />
-          <InputButton value={3} handleChangeRateNote={handleChangeRateNote} />
-          <InputButton value={4} handleChangeRateNote={handleChangeRateNote} />
-          <InputButton value={5} handleChangeRateNote={handleChangeRateNote} />
+          <InputButton value={1} setRateNote={setRateNote} />
+          <InputButton value={2} setRateNote={setRateNote} />
+          <InputButton value={3} setRateNote={setRateNote} />
+          <InputButton value={4} setRateNote={setRateNote} />
+          <InputButton value={5} setRateNote={setRateNote} />
         </div>
 
         <button className="bg-orange w-full py-3 rounded-3xl uppercase font-bold tracking-1 hover:bg-white hover:text-orange cursor-pointer transition duration-400" onClick={handleSubmit}>Submit</button>
